5c8b873a85aad92a9ff17d406305185228444477
Olivia <olivia@olivias-mbp.myfiosgateway.com>
2021-03-25 16:16:21 -0400

    graphql and images

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_ONi9AvTEp",
     "aws_user_pools_web_client_id": "43jjvd5vqcrs2mf4u7ltoo2670",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://stuyybadyffgbjmssdzlr7bdqm.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-bc37dx4vjzeyvjpwfn4aoqiwj4",
+    "aws_user_files_s3_bucket": "awstutorialdbf1cfb99ac44ee3a77c180430973edd173523-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
